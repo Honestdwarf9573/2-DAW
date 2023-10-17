0006623c9854aa4450cfaf0bd75eb9aef6bdf330
Enano Honesto <125579561+Honestdwarf9573@users.noreply.github.com>
2023-10-17 18:48:25 +0200

    Add files via upload

---
+++ Desarrollo Web Cliente/Tema 2/Ejercicios lunes 9 de octubre/Ejercicio_4.js
@@ -0,0 +1,23 @@
+var nombre = 'Juan';
+console.log(typeof nombre); // cadena de texto
+
+var edad = 30;
+console.log(typeof edad); // número
+
+var esMayor = true;
+console.log(typeof esMayor); // booleano
+
+var persona = {
+  nombre: 'María',
+  edad: 25
+};
+console.log(typeof persona); // objeto
+
+var numeros = [1, 2, 3];
+console.log(typeof numeros); // array
+
+var nulo = null;
+console.log(typeof nulo); //nulo
+
+var sinDefinir;
+console.log(typeof sinDefinir); // sin definir
