3a298a7d07819e6e5149a6abd70bfabb2dd0c3b2
Enano Honesto <125579561+Honestdwarf9573@users.noreply.github.com>
2023-11-23 08:35:59 +0100

    Delete Tema 2 directory

--- Tema 2/Ejercicios lunes 9 de octubre/Ejercicio_8.js
+++
@@ -1,9 +0,0 @@
-// Definimos una fecha concreta
-var fecha = new Date("2023-10-10");
-
-// Obtenemos el mes y el año de la fecha
-var mes = fecha.getMonth() + 1; // Sumamos 1 ya que los meses en JavaScript comienzan en 0 (enero)
-var anio = fecha.getFullYear();
-
-// Mostramos el resultado
-console.log("El mes es " + mes + " y el año es " + anio);
